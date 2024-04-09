31e38141f77f3fbc3276ba76c2162d21a9d82a36
dsprad99 <davislspradling@gmail.com>
2024-04-09 13:41:12 -0400

    updated project section

--- script.js
+++ script.js
@@ -12,4 +12,96 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
 }))
 
 
+document.getElementById('showButtonNonProfitSeeker').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreCardNonProfitSeeker');
+
+    if (learnMoreContainer.style.display === 'none') {
+        learnMoreContainer.style.display = 'flex'; 
+        learnMoreCardNonProfitSeeker.style.display = 'flex';
+    } else {
+        learnMoreContainer.style.display = 'none'; 
+    }
+    
+});
+
+document.getElementById('showButtonNeuseRiver').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreNeuseRiver');
+
+    if (learnMoreContainer.style.display === 'none') {
+        learnMoreContainer.style.display = 'flex'; 
+        learnMoreCardNonProfitSeeker.style.display = 'flex';
+    } else {
+        learnMoreContainer.style.display = 'none'; 
+    }
+    
+});
+
+document.getElementById('showButtonTheAdvisor').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreTheAdvisor');
+
+    if (learnMoreContainer.style.display === 'none') {
+        learnMoreContainer.style.display = 'flex'; 
+        learnMoreCardNonProfitSeeker.style.display = 'flex';
+    } else {
+        learnMoreContainer.style.display = 'none'; 
+    }
+    
+});
+
+document.getElementById('showButtonNFLCareer').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreNFLCareer');
+
+    if (learnMoreContainer.style.display === 'none') {
+        learnMoreContainer.style.display = 'flex'; 
+        learnMoreCardNonProfitSeeker.style.display = 'flex';
+    } else {
+        learnMoreContainer.style.display = 'none'; 
+    }
+    
+});
+
+document.getElementById('exitButtonNonProfit').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreCardNonProfitSeeker');
+
+    if (learnMoreContainer.style.display === 'flex') {
+        learnMoreContainer.style.display = 'none'; 
+        learnMoreCardNonProfitSeeker.style.display = 'none'; 
+    } 
+});
+
+document.getElementById('exitButtonNeuseRiver').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreNeuseRiver');
+
+    if (learnMoreContainer.style.display === 'flex') {
+        learnMoreContainer.style.display = 'none'; 
+        learnMoreCardNonProfitSeeker.style.display = 'none'; 
+    } 
+});
+
+document.getElementById('exitButtonTheAdvisor').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreTheAdvisor');
+
+    if (learnMoreContainer.style.display === 'flex') {
+        learnMoreContainer.style.display = 'none'; 
+        learnMoreCardNonProfitSeeker.style.display = 'none'; 
+    } 
+});
+
+document.getElementById('exitButtonNFL').addEventListener('click', function() {
+    var learnMoreContainer = document.getElementById('learnMoreContainer');
+    var learnMoreCardNonProfitSeeker = document.getElementById('learnMoreNFLCareer');
+
+    if (learnMoreContainer.style.display === 'flex') {
+        learnMoreContainer.style.display = 'none'; 
+        learnMoreCardNonProfitSeeker.style.display = 'none'; 
+    } 
+});
+
 
